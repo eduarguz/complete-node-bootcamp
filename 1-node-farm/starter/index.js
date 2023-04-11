df4d91541d36770d3d22f5212be47e264afc2234
Eduardo Guzmán H <eduarguzher@gmail.com>
2023-04-11 01:36:58 +0000

    12

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -25,22 +25,50 @@ const url = require('url');
 //      })
 // })
 
+const replaceTemplate = (template, product) => {
+    let output = template.replace(/{%PRODUCTNAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+    output = output.replace(/{%ID%}/g, product.id);
+
+    if (!product.organic){
+        output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    }
+    
+    return output;
+}
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 const dataObj = JSON.parse(data);        
+
+const templateOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8')
+const templateCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8')
+const templateProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8')
 
 
 const server = http.createServer((req, res) => {
     const pathName = req.url;
     
     if(pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview');
+        
+        const cardsHtml = dataObj.map(el => replaceTemplate(templateCard, el)).join("")
+        const output = templateOverview.replace(/{%PRODUCT_CARDS%}/, cardsHtml);
+
+        res.writeHead(200, {'Content-type': 'text/html'})
+        res.end(output);
     } else if (pathName === '/product') {
+
         res.end('This is the product');
     } else if (pathName === '/api'){
+
         res.writeHead(200, {'Content-type': 'application/json'})
         res.end(data);
     } else {
+
         res.writeHead(404, {
             'Content-type': 'text/html'
         });
